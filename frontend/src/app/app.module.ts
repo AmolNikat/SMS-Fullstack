33a517aeb2c03362662fcf7f108f4c0f58b837b8
AmolNikat <amolnikat13@gmail.com>
2020-10-24 13:02:25 +0530

    added dummy components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DateFilterComponent } from './shared/date-filter/date-filter.component';
+import { DataGridComponent } from './data-grid/data-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateFilterComponent,
+    DataGridComponent
   ],
   imports: [
     BrowserModule
